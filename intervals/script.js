a75b6ca4fbc7ae035c2691b8b4f5bcaa0504c968
Chelton Forbes <cj.p.forbes@gmail.com>
2023-08-03 14:47:43 -0400

    first commit

---
+++ intervals/script.js
@@ -0,0 +1,11 @@
+// number of seconds that have passed
+let time = 0;
+// reference to the <span> containing the number
+let counterElement = document.getElementById("time");
+
+setInterval(update,1000);
+
+function update() {
+    time++;
+    counterElement.innerHTML = time;
+}
